417e27e6873979faa465dd6c7ecf4139bc8dec7b
Zeeshan <zeeshanibrahim78@gmail.com>
2026-01-09 04:32:02 +0500

    ui-changes

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -6,11 +6,11 @@ export const Navbar = () => {
     <div className="backdrop-blur-xs">
 
     <div className="flex justify-between items-center mt-4 mr-10 ml-20 mb-4">
-      <h1 className="text-2xl text-white">MOHABBAT CLOTHING</h1>
+      <h1 className="text-2xl text-white font-montserrat">MOHABBAT CLOTHING</h1>
       <div className="flex gap-4 ">
-        <Link href="https://www.instagram.com/mohabbat_clothing/" target="_blank"><Image src="/insta-logo.png" className="rounded-4xl" alt="logo" width={25} height={25} /></Link>
-        <div className="bg-white rounded-4xl"><Link href=""><Image src="/fb-logo.svg" alt="logo" width={25} height={25} /></Link></div>
-        <Link href="/contact"><Image src="/x.jpg"  className="rounded-4xl" alt="logo" width={25} height={25} /></Link>
+        <Link href="https://www.instagram.com/mohabbat_clothing/" target="_blank"><Image src="/instagram-logo-fill.svg" className="invert rounded-4xl" alt="logo" width={30} height={30} /></Link>
+        <Link href=""><Image className="invert" src="/facebook-logo-fill.svg" alt="logo" width={30} height={30} /></Link>
+        <Link href=""><Image className="invert" src="/x-logo-fill.svg" alt="logo" width={30} height={30} /></Link>
       </div>
     </div>
     </div>
